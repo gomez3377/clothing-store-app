711648f38854fbc6bf8b11d3da25762746fcf267
gomez3377 <jongomez3377@gmail.com>
2023-02-08 21:31:25 -0700

    added a nav bar and added react routing in the app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
-import Directory from './components/directory/directory.component';
-
+import { Routes, Route} from "react-router-dom";
+import Home from "./routes/home/home.component";
+import  Navigation  from "./routes/navigation/navigation.component";
+import Shop from "./routes/shop/shop.component";
 
 
 const App = () => {
-
-  
-  
-
   return (
-    <div>
-      <Directory />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home/>} />
+        <Route path="shop" element={<Shop/>} />
+
+        </Route>
+    </Routes>
   );
 }
 

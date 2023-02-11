7cf8848928571c4d5f62345cff720f64ab4df2fc
gomez3377 <jongomez3377@gmail.com>
2023-02-11 03:49:54 -0700

    created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,29 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from "react";
 
+import {
+ 
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  
+} from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user)
-        
-    }
+  
+
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
   return (
     <div>
-        <h1>Sign in here</h1>
-        <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <h1>Sign in here</h1>
+      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <SignUpForm/>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,37 +1,45 @@
-import { async } from "@firebase/util";
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-} from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7BKz2e4y3lVm4vILJs6a2PhYOVrWNhDA",
-  authDomain: "clothing-store-db-20433.firebaseapp.com",
-  projectId: "clothing-store-db-20433",
-  storageBucket: "clothing-store-db-20433.appspot.com",
-  messagingSenderId: "713285853373",
-  appId: "1:713285853373:web:ab515e86381a0524b6d7d4",
+    apiKey: "AIzaSyC7BKz2e4y3lVm4vILJs6a2PhYOVrWNhDA",
+    authDomain: "clothing-store-db-20433.firebaseapp.com",
+    projectId: "clothing-store-db-20433",
+    storageBucket: "clothing-store-db-20433.appspot.com",
+    messagingSenderId: "713285853373",
+    appId: "1:713285853373:web:ab515e86381a0524b6d7d4"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-  prompt: "select_account",
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, "users", userAuth.uid);
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
 
@@ -44,11 +52,18 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
-      console.log("error creating the user", error.message);
+      console.log('error creating the user', error.message);
     }
   }
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
 };

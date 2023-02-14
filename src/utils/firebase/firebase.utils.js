2a8c381b0ec749d7bb0997a3130ac325dcd49621
Jonathon Gomez <jongomez3377@gmail.com>
2023-02-14 05:04:39 -0700

    Linked a database of the products to the app, and  created nested and dynamic routes for each category of products

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,4 @@
-
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithRedirect,
@@ -8,17 +7,26 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
-} from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7BKz2e4y3lVm4vILJs6a2PhYOVrWNhDA",
-    authDomain: "clothing-store-db-20433.firebaseapp.com",
-    projectId: "clothing-store-db-20433",
-    storageBucket: "clothing-store-db-20433.appspot.com",
-    messagingSenderId: "713285853373",
-    appId: "1:713285853373:web:ab515e86381a0524b6d7d4"
+  apiKey: "AIzaSyC7BKz2e4y3lVm4vILJs6a2PhYOVrWNhDA",
+  authDomain: "clothing-store-db-20433.firebaseapp.com",
+  projectId: "clothing-store-db-20433",
+  storageBucket: "clothing-store-db-20433.appspot.com",
+  messagingSenderId: "713285853373",
+  appId: "1:713285853373:web:ab515e86381a0524b6d7d4",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -26,7 +34,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: 'select_account',
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
@@ -37,13 +45,49 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef)
+
+  const queryShopshot = await getDocs(q)
+
+
+  const categoryMap = queryShopshot.docs.reduce((acc,docSnapshot) => {
+    const { title, items } = docSnapshot.data()
+    acc[title.toLowerCase()] = items;
+    return acc
+  }, {})
+
+
+  return categoryMap
+
+
+};
+
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
 ) => {
   if (!userAuth) return;
 
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
 
@@ -59,7 +103,7 @@ export const createUserDocumentFromAuth = async (
         ...additionalInformation,
       });
     } catch (error) {
-      console.log('error creating the user', error.message);
+      console.log("error creating the user", error.message);
     }
   }
 
@@ -73,11 +117,12 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
+  if (!email || !password) return;
 
-    return await signInWithEmailAndPassword(auth, email, password)
-}
+  return await signInWithEmailAndPassword(auth, email, password);
+};
 
-export const signOutUser = async () => await signOut(auth)
+export const signOutUser = async () => await signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback)
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
